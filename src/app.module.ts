7946b19c6c452c5670e85b8747ffb6d90ec22f7d
million-art <millionmulugeta09@gmail.com>
2024-11-29 16:29:50 +0300

    db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development'], // Load environment variables
+      isGlobal: true, // Make ConfigModule globally available
+    }),
+    TypeOrmModule.forRootAsync({
+       inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql', // Database type
+        host: 'localhost', // Database host from environment variable
+        port: 3306, // Database port from environment variable
+        username: 'root', // Database username
+        password: '', // Database password
+        database:  'microservice', // Database name
+        autoLoadEntities: true, // Automatically load entities from your directories (remove it on production)
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'], // Paths to your entities
+        synchronize: true, // Auto sync the schema; use false in production
+      })
+
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
